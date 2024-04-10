f3711567dcbe7d9e210445aec90ce7044d9ed8ce
dmartens3 <dmartens3@rrc.ca>
2024-04-10 17:49:28 -0500

    Added validation to province table

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,8 @@ class User < ApplicationRecord
   message: "Email format is invalid" }
   validates :address, presence: true
   validates :city, presence: true
-  validates :province, presence: true, format: { with: /\A(?:AB|BC|MB|NB|NL|NS|NT|NU|ON|PE|QC|SK|YT)\z/,
-  message: "Must be an uppercase two-character representation of a Canadian province" }
+  # validates :province, presence: true, format: { with: /\A(?:AB|BC|MB|NB|NL|NS|NT|NU|ON|PE|QC|SK|YT)\z/,
+  # message: "Must be an uppercase two-character representation of a Canadian province" }
   validates :postal_code, presence: true, format: { with: /\A([ABCEGHJKLMNPRSTVXY]\d[ABCEGHJKLMNPRSTVWXYZ])[\s-]?(\d[ABCEGHJKLMNPRSTVWXYZ]\d)\z/i,
   message: "Must be a valid Canadian postal code" }
 end
