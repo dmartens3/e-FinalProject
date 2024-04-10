9d9b7fc3df5361032a1d4ee37e5327234e8446d0
dmartens3 <dmartens3@rrc.ca>
2024-04-10 18:22:05 -0500

    Edited migration file to drop old column

--- db/migrate/20240410231759_add_province_reference_to_user.rb
+++ db/migrate/20240410231759_add_province_reference_to_user.rb
@@ -1,5 +1,7 @@
 class AddProvinceReferenceToUser < ActiveRecord::Migration[7.1]
   def change
+    remove_column :users, :province
     add_reference :users, :province, null: false, foreign_key: true
+    add_foreign_key :users, :province, column: :province_id
   end
 end
