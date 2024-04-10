d45944616d0fd01a780a48d90acfe303bd361289
dmartens3 <dmartens3@rrc.ca>
2024-04-10 17:41:00 -0500

    $ rails db:migrate (create provinces)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_182422) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_223332) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_182422) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.decimal "gst"
+    t.decimal "pst"
+    t.decimal "hst"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
