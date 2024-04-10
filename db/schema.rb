ab9f5a04d45f80af1a3d00c645f7a7913ea35f2b
dmartens3 <dmartens3@rrc.ca>
2024-04-10 18:57:44 -0500

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_10_231759) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_235603) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -27,6 +27,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_231759) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "provinces", force: :cascade do |t|
@@ -56,4 +66,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_10_231759) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "categories"
 end
