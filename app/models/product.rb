cf773da6bf539d09be3db5900483d4b41065df9f
dmartens3 <dmartens3@rrc.ca>
2024-04-10 20:44:53 -0500

    Adjusted thumbnail size

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   #allow an image to be attached and returns a thumbnail when you ask for the thumb variant.
   has_many_attached :images do |attachable|
-    attachable.variant :thumb, resize_to_limit: [50, 50]
+    attachable.variant :thumb, resize_to_limit: [100, 100]
     attachable.variant :small, resize_to_limit: [200, 200]
   end
 
