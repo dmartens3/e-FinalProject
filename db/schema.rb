06e96edb2b55f929ea1fb94a39933fcdfce1a0e2
dmartens3 <dmartens3@rrc.ca>
2024-04-18 14:44:50 -0500

    $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_11_002056) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_194212) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -73,6 +73,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_11_002056) do
     t.decimal "gst"
     t.decimal "pst"
     t.decimal "hst"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "statuses", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
