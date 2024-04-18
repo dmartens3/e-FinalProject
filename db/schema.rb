24c6f28a5d162b29f7ab0397c976d888bbce4bd9
dmartens3 <dmartens3@rrc.ca>
2024-04-18 14:55:54 -0500

    $ rails g scaffold order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_18_194212) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_195353) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_194212) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "status_id", null: false
+    t.integer "user_id", null: false
+    t.decimal "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["status_id"], name: "index_orders_on_status_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -102,5 +112,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_18_194212) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "statuses"
+  add_foreign_key "orders", "users"
   add_foreign_key "products", "categories"
 end
